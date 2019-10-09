bf6d6ff4f5d668699ab6df65d54d7284af348920
Ansuman Acharya <nitinansu12@gmail.com>
2019-10-09 21:26:49 +0530

    Lane detection using Hough lines

--- pynfs.py
+++ pynfs.py
@@ -9,11 +9,24 @@ bbox = {'top': 50, 'left': 0, 'width': 800, 'height': 600}
 k = PyKeyboard()
 
 
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            #print(line)
+            coordinates = line[0]
+            cv2.line(img, (coordinates[0], coordinates[1]), (coordinates[2], coordinates[3]), [255, 255, 255], 3)
+    except:
+        pass
+
+
 def process(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    processed_image = cv2.Canny(gray_image, 100, 200)
-    vertices = np.array([[0, 600], [0, 400], [250, 325], [450, 325], [800, 400], [800, 600]])
+    processed_image = cv2.Canny(gray_image, 200, 300)
+    processed_image = cv2.GaussianBlur(processed_image, (5, 5), 0)
+    vertices = np.array([[0, 600], [0, 400], [250, 300], [450, 300], [800, 400], [800, 600]])
     processed_image = roi(processed_image, [vertices])
+    lines = cv2.HoughLinesP(processed_image, 1, np.pi / 180, 100, 20, 15)
+    draw_lines(processed_image, lines)
     return processed_image
 
 
